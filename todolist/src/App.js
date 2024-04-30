c7b126a0c4f6a356c8691436c5ee6524a460cc3b
Bita Fotovvat <bita.ftvt@gmail.com>
2024-04-30 17:21:29 -0400

    completed

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,39 +1,39 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 function App() {
-  const [newItem, setNewItem] = useState("");
+  const [newItem, setNewItem]= useState("");
   const [items, setItems] = useState([]);
 
- function addItem(){
-  if(!newItem){
-    alert("Please add item...");
-    return;
+  function addItem(){
+    if(!newItem){
+      alert("Please enter a task.");
+      return;
+    }
+    const item = {
+      id: Math.floor(Math.random()*1000),
+      value: newItem
+    };
+    setItems(oldList=>[...oldList, item]);
+    setNewItem("");
   }
-  const item = {
-    id: Math.floor(Math.random()*1000),
-    value: newItem
+  function deleteItem(id){
+    const newArray = items.filter(item=> item.id !==id);
+    setItems(newArray);
   }
-  setItems(oldList=>[...oldList, item]);
-  console.log(items);
-  setNewItem("");
- }
- function deleteItem(id){
-  const newArray = items.filter(item=>item.id!==id);
-  setItems(newArray);
- }
+  
   return (
     <div className="App">
       <h1>My To Do List</h1>
-      <input 
+      <input
       type="text"
-      placeholder='Add task'
-      value={newItem}
+      placeholder="Add a task..."
+      value= {newItem}
       onChange={e=>setNewItem(e.target.value)}
       />
       <button onClick={()=>addItem()}>Add</button>
       <ul>
         {items.map(item=>{
-          return(<li key = {item.id} onClick={()=>deleteItem(item.id)}>{item.value}</li>);
+          return(<li key={item.id}>{item.value} <button onClick={()=>deleteItem(item.id)}>Delete</button></li>)
         })}
       </ul>
     </div>
